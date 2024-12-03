b8ca265172b8e163f1beed1000d1a2dd8ba91dd2
Marcos <marcos.vtl@hotmail.com>
2024-12-03 17:04:00 -0300

    swagger

--- Dashboard/Program.cs
+++ Dashboard/Program.cs
@@ -1,27 +1,44 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Adicione os serviços do Swagger
+        builder.Services.AddSwaggerGen();
+
+        // Adicione os serviços de controllers
+        builder.Services.AddControllersWithViews();
+
+        var app = builder.Build();
+
+        // Configure o Swagger para ser exibido
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger(); // Habilita o Swagger UI
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+                c.RoutePrefix = string.Empty; // Tornar o Swagger acessível na raiz
+            });
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
